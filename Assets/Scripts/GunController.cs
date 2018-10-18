e52bd3925b799171bbedf90c2bce6fdf75c4d105
Glen <1008085@student.sae.edu.au>
2018-10-18 16:59:38 +1100

    CommentedCode

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
-	//Note: this script can be used on different types of guns by changing the variables
+	//Note: this script can be used on different types of guns/powerUps by changing the variables
 	public bool isFiring;
 
 	public BulletController bullet;
 	public float bulletSpeed;
-
+	//Calculates
 	public float timeBetweenShots = 0.3333f;
 	public float shotCounter;
+	//point in time
 	private float timestamp;
 
 	public Transform bulletSpawnPoint;
@@ -19,6 +20,7 @@ public class GunController : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
+		//set the firing bool to false at the beginning of the game
 		isFiring = false;
 	}
 	
@@ -27,17 +29,20 @@ public class GunController : MonoBehaviour
 	{
 		//if the gun is firing
 		if (isFiring)
-
 		{
+			//if the player is pressing and holding the left mouse button down, the game gets the current time, checks if it is less than equal to the timestamp variable then;
 			if (Time.time >= timestamp && (Input.GetMouseButton(0)))
 			{
+				//sets the firing bool to true
 				isFiring = true;
+				//Spawns the bullet at the location of the Bullet Spawn Point. 
 				BulletController newBullet = Instantiate(bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+				//Sets the timestamp to the current game time while adding the timeBetweenShots variable
 				timestamp = Time.time + timeBetweenShots;
+				//Sets the spawned bullets speed to the user defined bullet speed variable in the editor.
 				newBullet.speed = bulletSpeed;
 			}
 		}
-
 	
 	}
 }
