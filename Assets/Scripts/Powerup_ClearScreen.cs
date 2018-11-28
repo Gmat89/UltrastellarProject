32e9c0537f55bbb1912faabb3abee417b1de0048
Dylan Webb <Dylanwebb5@hotmail.com>
2018-11-28 17:26:14 +1100

    Added in powreup prefabs

---
+++ Assets/Scripts/Powerup_ClearScreen.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup_ClearScreen : MonoBehaviour
+{
+
+    public GameObject pickupEffect;
+    public ParticleSystem pickupParticle;
+    private float powerupDuration = 4.0f;
+
+    private GameObject[] gameObjects;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        pickupParticle = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    public void DestoryAllEnemies()
+    {
+        gameObjects = GameObject.FindGameObjectsWithTag("Enemy");
+
+        for (var i = 0; i < gameObjects.Length; i++)
+        {
+
+            Destroy(gameObjects[i]);
+
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            pickupParticle.Clear();
+            //DestoryAllEnemies();
+            StartCoroutine(Pickup());
+        }
+    }
+
+    IEnumerator Pickup()
+    {
+        //Spawn Effect when pick is collected
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        //Apply the effect to the Player
+        DestoryAllEnemies();
+
+        //Disable the mesh renderer when pickup is obtained
+        GetComponent<MeshRenderer>().enabled = false;
+        //Disable collider when the pickup is obtained.
+        GetComponent<Collider>().enabled = false;
+        //Destroy the particle
+        Destroy(pickupParticle);
+
+        //Wait x amount of seconds
+        yield return new WaitForSeconds(powerupDuration);
+
+        //Destory the Pickup object
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Powerup_PlayerSpeedUp.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup_PlayerSpeedUp : MonoBehaviour
+{
+    private PlayerController player;
+    public float ROFPower;
+    public GameObject pickupEffect;
+    public ParticleSystem pickupParticle;
+    public float powerupDuration = 4.0f;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        pickupParticle = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            pickupParticle.Clear();
+            StartCoroutine(Pickup());
+        }
+    }
+
+    IEnumerator Pickup()
+    {
+        //Spawn Effect when pick is collected
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        //Apply the effect to the Player
+        player.moveSpeed += ROFPower;
+
+        if (player.moveSpeed < 5)
+        {
+            player.moveSpeed = Mathf.Clamp(player.moveSpeed, 0, player.moveSpeed);
+        }
+        if (player.moveSpeed > 10)
+        {
+            player.moveSpeed = Mathf.Clamp(player.moveSpeed, 0, player.moveSpeed);
+        }
+
+        //Disable the mesh renderer when pickup is obtained
+        GetComponent<MeshRenderer>().enabled = false;
+        //Disable collider when the pickup is obtained.
+        GetComponent<Collider>().enabled = false;
+        //Destroy the particle
+        Destroy(pickupParticle);
+
+        //Wait x amount of seconds
+        yield return new WaitForSeconds(powerupDuration);
+        //Reverse the effect on the player (reset back to default firing rate)
+        player.moveSpeed -= ROFPower;
+
+        //Destory the Pickup object
+        Destroy(gameObject);
+    }
+
+}
