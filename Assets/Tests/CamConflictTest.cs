24165cc3427c55e48dd8f70d9282c986e88fc006
Cameron Bonde <c.bonde@sae.edu>
2018-11-29 11:05:54 +1100

    Test conflicts

---
+++ Assets/Tests/CamConflictTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamConflictTest : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		print("cam test");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
