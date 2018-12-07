8c71f9f0101c78c274e34d5486510b62f05e5d12
Glen <1008085@student.sae.edu.au>
2018-12-07 15:02:55 +1100

    Refixed the screen wobble effect

---
+++ Assets/SkyBoxWobble.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxWobble : MonoBehaviour
+{
+	public Material skyBox;
+
+	public float scrollSpeed;
+	// Use this for initialization
+	void Start ()
+	{
+		RenderSettings.skybox = skyBox;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float offset = Time.time * scrollSpeed;
+
+		skyBox.mainTextureOffset = new Vector2(offset,0);
+	}
+}
